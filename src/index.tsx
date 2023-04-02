2d68468acd32fc3ce2a1b1684a8bdc4e8bfcf213
narumir <narumir@narumir.io>
2023-04-02 09:15:59 +0000

    apply react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
+const element = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(element);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
