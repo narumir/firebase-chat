bde3e7f4fce748aa026ea861442aa8f6e44aa8c4
narumir <narumir@narumir.io>
2023-04-17 06:19:14 +0000

    create chat room modal

--- src/components/SidePanel/ChatRooms.tsx
+++ src/components/SidePanel/ChatRooms.tsx
@@ -1,8 +1,68 @@
+import {
+  useCallback,
+  useState,
+} from "react";
+import {
+  Button,
+  Form,
+  Modal,
+} from "react-bootstrap";
+import {
+  useForm,
+} from "react-hook-form";
+import {
+  FaRegSmileWink,
+  FaPlus,
+} from "react-icons/fa";
 
+type CreateChatRoomFormData = {
+  name: string;
+  description: string;
+}
 
 function ChatRooms() {
+  const [isShow, setModalShow] = useState<boolean>(false);
+  const onModalOpen = useCallback(() => {
+    setModalShow(true);
+  }, []);
+  const onModalClose = useCallback(() => {
+    setModalShow(false);
+  }, []);
+  const { register, handleSubmit, formState: { errors } } = useForm<CreateChatRoomFormData>();
+  const onChatroomSubmit = handleSubmit(async (data) => {
+    console.log(data);
+  });
   return (
-    <></>
+    <div>
+      <div style={{ position: "relative", width: "100%", display: "flex", alignItems: "center" }}>
+        <FaRegSmileWink style={{ marginRight: "3px" }} />
+        CHAT ROOMS &nbsp; (1)
+        <FaPlus style={{ position: "absolute", right: "0", cursor: "pointer" }} onClick={onModalOpen} />
+      </div>
+      <Modal show={isShow} onHide={onModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create a chat room</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={onChatroomSubmit}>
+          <Modal.Body>
+            <Form.Group controlId="room-name">
+              <Form.Label>방 이름</Form.Label>
+              <Form.Control {...register("name", { required: true })} type="text" placeholder="Enter a chat room name" />
+              {errors.name?.type === "required" && <p>This name feild is required</p>}
+            </Form.Group>
+            <Form.Group controlId="room-description">
+              <Form.Label>방 설명</Form.Label>
+              <Form.Control {...register("description", { required: true })} type="text" placeholder="Enter a chat room description" />
+              {errors.description?.type === "required" && <p>This description feild is required</p>}
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" type="submit">Create</Button>
+            <Button variant="secondary" onClick={onModalClose}>Close</Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </div>
   );
 }
 
